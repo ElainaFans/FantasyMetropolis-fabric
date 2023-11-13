816d37a759a6ce7b413e8f396762d0125068535c
Usagi no Niku <chenbohan911@163.com>
2023-11-13 09:16:06 +0800

    add files

---
+++ src/main/java/turou/fantasy_metropolis/fabric/FantasyMetropolisDataGenerator.java
@@ -0,0 +1,11 @@
+package turou.fantasy_metropolis.fabric;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class FantasyMetropolisDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
